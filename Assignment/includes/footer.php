dac2385f5045dc1fae2aa5aebb9bcc32e4c72e1c
William Edwards <78228646+Rocka33r@users.noreply.github.com>
2022-04-14 01:32:01 +0100

    Add files via upload

---
+++ Assignment/includes/footer.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-sm footer" id="footer">
+    <div class="container">
+        <div class="navbar-text float-left copyright">
+            <p><a class="footer-link" href="https://www.linkedin.com/in/william-edwards-6a751b19a/" target="_blank">By William Edwards</a>
+													<a class="footer-link" href="javascript:changeColours()">--- Change Theme</a>
+													<a class="footer-link" href="https://github.com/Rocka33r/3DWebApps" target="_blank">--- Github</a>
+        </div>
+    </div>
+</nav>
---
+++ Assignment/includes/header.php
@@ -0,0 +1,32 @@
+<nav class="navbar sticky-top navbar-expand-sm navbar_coca_cola main-colour">
+    <div class="container">
+        <div class="logo">
+            <a class="navbar-brand" href="javascript:showHome()">
+                <h1>1</h1>
+                <h1>oca</h1>
+                <h2>Cola</h2>
+                <h3>World Wide</h3>
+                <p>Real Magic.</p>
+            </a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"><i class="fas fa-bars" style="color: white"></i></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="javascript:showHome()"><i class="fa-solid fa-house"></i></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">Products</a>
+                    <div class="dropdown-menu dropdown-menu-right main-colour">
+                        <a class="dropdown-item" href="javascript:showModel('coke')" >Coca Cola</a>
+                        <a class="dropdown-item" href="javascript:showModel('sprite')">Sprite</a>
+                        <a class="dropdown-item" href="javascript:showModel('fanta')">Fanta</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>

cff523e84afee74fc8f6fbdbee9ded1004465c74
William Edwards <78228646+Rocka33r@users.noreply.github.com>
2022-04-14 01:30:38 +0100

    Delete Assignment directory

--- Assignment/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<nav class="navbar navbar-expand-sm footer" id="footer">
-    <div class="container">
-        <div class="navbar-text float-left copyright">
-            <p><a class="footer-link" href="https://www.linkedin.com/in/william-edwards-6a751b19a/" target="_blank">By William Edwards</a>
-													<a class="footer-link" href="javascript:changeColours()">--- Change Theme</a>
-        </div>
-    </div>
-</nav>
